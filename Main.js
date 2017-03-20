539610b69b1b144429c208d1dfed503f5c46cbe3
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-20 13:20:37 +0700

    Add tabbar

--- Main.js
+++ Main.js
@@ -1,37 +1,34 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Actions, Scene, Router, Switch, Modal } from 'react-native-router-flux';
+import About from './scenes/About';
+import Question from './scenes/Question';
 
-class Main extends Component {
-
+class TabIcon extends Component {
   render() {
-
+    const title = this.props.title;
     return (
-
-      <View style = {styles.container}>
-        <Text style = {styles.welcome}>
-          Welcom to React Native!
-        </Text>
-      </View>
+      <Text>{title}</Text>
     );
-
   }
 }
 
-const styles = StyleSheet.create({
+class Main extends Component {
+componentWillMount(){
+  this.scenes = Actions.create(
+    <Scene key ="root" tabs={true}>
+      <Scene key ="tabbar" tabs={true} tabBarStyle={{backgroundColor:'#f7f7f7'}}>
+        <Scene key="Questions" component={Question} title="Questions" icon={TabIcon} hideNavbar={true}/>
+        <Scene key="About" component={About} title="About" icon={TabIcon} hideNavbar={true}/>
+      </Scene>
+    </Scene>
+  );
 
-  container:{
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
+}
 
-  welcome: {
-    fontSize:20,
-    textAlign: 'center',
-    margin: 10
+  render() {
+    return <Router scenes={this.scenes}/>
   }
-
-});
+}
 
 module.exports = Main;
