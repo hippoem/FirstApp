92b3d7ec33ea846529dab67e1b6b662193e084a0
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-20 14:08:50 +0700

    Header Container About Page

--- scenes/About.js
+++ scenes/About.js
@@ -1,25 +1,36 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {Header, Body, Title, Container, Content} from 'native-base';
 
 export default class About extends Component {
+
+  renderHeader(){
+    const {title} = this.props;
+    return(
+      <Header>
+        <Body>
+          <Title>{title}</Title>
+        </Body>
+      </Header>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          About Page
-        </Text>
-      </View>
+      <Container>
+        {this.renderHeader()}
+        <Content style={styles.container}>
+          <Text style={styles.welcome}>
+            About Page
+          </Text>
+        </Content>
+      </Container>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
   welcome:{
     fontSize: 20,
     textAlign: 'center',
