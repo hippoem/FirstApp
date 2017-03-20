c50a51332f121c97b2a14098bab39886efc8b843
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-20 14:28:15 +0700

    show title author detail

--- scenes/QuestionDetail.js
+++ scenes/QuestionDetail.js
@@ -21,11 +21,15 @@ export default class QuestionDetail extends Component {
   }
 
   render(){
+    const {title, author} = this.props.question;
+    //const title = this.props.question.title;
+    //const author = this.props.question.author;
     return(
       <Container>
         {this.renderHeader()}
         <Content>
-          <Text>Question Detail Page</Text>
+          <Text>Title: {title}</Text>
+          <Text>Author: {author}</Text>
         </Content>
       </Container>
     );
